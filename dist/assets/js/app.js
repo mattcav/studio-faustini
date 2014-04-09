dc60ee799691cfc74737236acceb06f326b2ef1a
mattcav <matteo@mced.it>
2014-04-09 17:24:40 +0200

    adding favicon & removing all unused foundation css stuff

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -332,3 +332,24 @@ c,d){return b("addClass",a,f(c,"-add"),d)},beforeRemoveClass:function(b,c,d){var
 
     }])
 
+// ios fix
+    (function(doc) {
+ 
+        var addEvent = 'addEventListener',
+        type = 'gesturestart',
+        qsa = 'querySelectorAll',
+        scales = [1, 1],
+        meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
+         
+        function fix() {
+        meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
+        doc.removeEventListener(type, fix, true);
+        }
+         
+        if ((meta = meta[meta.length - 1]) && addEvent in doc) {
+        fix();
+        scales = [.25, 1.6];
+        doc[addEvent](type, fix, true);
+        }
+     
+    }(document));
